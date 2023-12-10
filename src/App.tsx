42c50892eb59ae242a6d80e304cb465e5a9ebec2
Karthic Raja <karthicraja@neulancers.com>
2023-12-10 12:30:12 +0530

    Build a Responsive layout using Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-	return <Button colorScheme="blue">Button</Button>;
+	return (
+		<>
+			<div className="grid md:grid-cols-2 grid-rows-2 grid-row-2">
+				<div className="bg-green-400 col-start-1 col-span-2">nav</div>
+				<div className="bg-red-400 hidden md:inline">aside</div>
+				<div className="bg-orange-400 xs:max-md:col-span-2">main</div>
+
+			</div>
+
+		</>
+	);
 }
 
 export default App;
